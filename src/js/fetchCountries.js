54846473cd6ca8881dd5abc3e1d2b2070514289b
gash94 <104354207+gash94@users.noreply.github.com>
2022-10-19 12:46:18 +0200

    update loading and error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,30 @@
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
 
 const COUNTRY_API_URL = 'https://restcountries.com/v3.1/name/';
 
 const fetchCountries = name => {
-  Loading.circle();
+  Block.standard('.js-element', {
+    backgroundColor: 'rgba(255, 0, 0, 0)',
+    svgSize: '56px',
+  });
   return fetch(
     `${COUNTRY_API_URL}${name}?fields=name,capital,population,flags,languages`
   )
+    .then(response => {
+      console.log(response);
+      if (response.status === 404) {
+        return Promise.reject(
+          new Error('Currently, restcountries page is not available')
+        );
+      }
+      return response;
+    })
     .then(response => response.json())
+    .catch(err => {
+      console.error(err);
+    })
     .finally(() => {
-      Loading.remove(300);
+      Block.remove('.js-element', 300);
     });
 };
 
